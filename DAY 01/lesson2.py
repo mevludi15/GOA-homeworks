dcfaced281bb7157373379d23f562ff62beeeaa8
mevludi15 <mevludigamer08@gmail.com>
2024-08-27 10:12:10 -0700

    first commit

---
+++ DAY 01/lesson2.py
@@ -0,0 +1,8 @@
+name = "mevludi"
+age = 15
+height = 179
+surname = "tavadze"
+
+print("my name is" + " " + name + " " + surname)
+print("I am" + " " + str(age) + " " + "years old") 
+print("my height is" + " " + str(height) )
