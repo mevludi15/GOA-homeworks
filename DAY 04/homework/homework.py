3167c25dd35ec73dafee8cd8358dbcec4e006479
mevludi15 <mevludigamer08@gmail.com>
2024-10-06 00:20:41 -0700

    second commit

---
+++ DAY 04/homework/homework.py
@@ -0,0 +1,25 @@
+points = 50 
+points = 60
+points = 32 
+points = 56
+print(points)
+#პირველი დავალება
+
+age = 156
+print(age)
+#შესწორებულად გადავწერე 
+
+a = 5
+b = 10
+print(a+b)
+print(a-b)
+print(a*b)
+print(b/a)
+
+#შევასრულე ყველა მათემატიკური მოქმედებები 
+
+#name == "mevludi'
+#print((name)
+
+name = "mevludi"
+print(name)
