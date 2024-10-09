5f5ed5a6274cccb43691e905701a8d37b731dbb1
mevludi15 <mevludigamer08@gmail.com>
2024-10-09 05:43:08 -0700

    second commit

---
+++ DAY 10/Homework/homework.py
@@ -0,0 +1,22 @@
+print(5 == 10)
+print(30 < 15)
+print(12 > 7)
+print(21 <= 21)
+print(35 >= 42) 
+
+#პროგრამირებაში არაფერია შეუძლებელი ადამიანს აქ არაქვს შეზღუდვა მას შეუძლია გააკეთოს ის რაც უნდა 
+#იქამდე სადამდეც მისი ფანტაზია გაწვდება  
+
+#temp = 30
+
+#if temp > 0 and temp <30:
+#    print("the temperature is good")
+#else:
+#    print("the temperature is bad")
+
+sunny = "true"
+
+if sunny: 
+    print("it is sunny outside")
+else:
+    print("it is cloudy outside") 
