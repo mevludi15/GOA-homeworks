53cd0a52e6559aba0e1b1a22f4a17f58c8e856c2
mevludi15 <mevludigamer08@gmail.com>
2024-10-13 01:42:49 -0700

    second commit

---
+++ DAY 12/Classwork/classwork.py
@@ -0,0 +1,27 @@
+print((5>4) and False)#გამოიტანს false
+print((5>2) and True)#გამოიტანს ture
+print(False or True)#გამოიტანს ture 
+print(False or (6>10))#გამოიტანს false
+print(not True and True)#გამოიტანს false
+print(not False and True)#გამოიტანს true
+print(not True or False)#გამოიტანს false
+print(not True or True)#გამოიტანს true
+
+
+print(True and False)
+print(True and True)
+print(not False and True)
+print((20>15) and False)
+print(not True and False)
+
+print(True or True)
+print(True or False)
+print(not True or False)
+print((16<5) or True)
+print(not False or False)
+
+print((5<=5) and (20>30))
+print((13>5) and (13>=14))
+print((3*3>9) or (9<7))
+print((50/5<8) or (20>3))
+print((23+4>50) or (7*3<12/2))
---
+++ DAY 12/Homework/homework.py
@@ -0,0 +1,23 @@
+print(True and True)#გამოიტანს true
+print(False and False)#გამოიტანს false 
+print(False and False and True and False)#გამოიტანს false
+print(False and True)#გამოიტანს false
+print((50<20) and (20/10) and False)#გამოიტანს false
+print((21+7<50/2) and True and True and (3*2<20))#გამოიტანს false
+print(not True and False and not False)#გამოიტანს false
+print((21+3+4/2>20) and not True)#გამოიტანს false
+print((20.7+0.7+0.3<20) and False and not False)#გამოიტანს false
+print(True and True and not False and True and True and not False and (20>3) and True and (3+3>5))#გამოიტანს true
+
+
+
+print(True or True)
+print(False or not True)
+print(False or False or False or not False)
+print((30<20) or not True)
+print((20/2>23) or (23>21))
+print(True or False or True or False or True or False or not True)
+print(not True or not True or (30>21) or False)
+print((23*4>100) or not True)
+print((20 <= 19) or (20>17))
+print(False or False or not True or (32<20) or False or (20<30))
