befd663a750488748a0f00533bb5d2be39e80c43
mevludi15 <mevludigamer08@gmail.com>
2024-10-17 05:34:24 -0700

    second commit

---
+++ DAY 14/Classwork/classwork.py
@@ -0,0 +1,8 @@
+for i in range(100):
+    print(i)
+
+
+x = 0
+while x < 100:
+    print(x)
+    x = x + 1 
---
+++ DAY 14/Homework/homework.py
@@ -0,0 +1,23 @@
+# x = 0 
+# while x < 5:
+#     print("hello world")
+#     x = x + 1
+
+# for i in range(10):
+#     print("goodbye world")
+
+# for i in range(100):
+#     print(i)
+
+# y = 0
+# while y < 100:
+#     print(y)
+#     y = y + 1
+
+# for i in range(49,51):
+#     print(i)
+
+z = 16
+while z < 57:
+    print(z)
+    z = z + 1
