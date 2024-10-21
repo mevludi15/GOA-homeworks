7d14a8a0305b475dfa3e96da2cb722a218c3f643
mevludi15 <mevludigamer08@gmail.com>
2024-10-21 08:35:48 -0700

    second commit

---
+++ DAY 16/Classwork/classwork.py
@@ -0,0 +1,35 @@
+# for i in range(0,200,6):
+#     print(i)
+
+# str1 = "goodbye world"
+
+# for i in str1:
+#     print(i)
+
+# for i in range (1000,0,-1):
+#     print(i)
+
+# age = int(input("enter your age"))
+
+# if age < 18:
+#     print("you get 50% Discount")
+# else:
+#     print("you don't get discount")
+
+# age = int(input("enter your age"))
+
+# if age < 18:
+#     print("you get 50% Discount")
+# elif age == 18:
+#     print("you get 25% Discount")
+# else:
+#     print("you don't get discount")
+
+
+age = int(input("enter your age"))
+is_student = False
+
+if age < 18 or is_student:
+    print("you get 50% Discount")
+else:
+    print("you don't get Discount")
---
+++ DAY 16/Homework/homewrok.py
@@ -0,0 +1,15 @@
+for i in range(1,11,):
+    print(i * 2)
+
+x = 1
+while x < 11:
+    print(x * 2)
+    x = x + 1
+    
+num1 = int(input("enter number 0 - 100"))
+
+if num1 >= 90:
+    print("you got Grade A")
+else:
+    print("you got Grade B")
+ 
