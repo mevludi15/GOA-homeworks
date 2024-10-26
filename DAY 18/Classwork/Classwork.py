78fec4ce63456a055dc44691715336abf98b1673
mevludi15 <mevludigamer08@gmail.com>
2024-10-26 13:04:00 +0400

    second commit

---
+++ DAY 18/Classwork/Classwork.py
@@ -0,0 +1,26 @@
+list1 = ["item1", "item2", 13, "item3", "item4"]
+#           0        1      2     3        4
+list2 = [21, 15, 5.0, "ice",True ]
+#        0    1   2     3    4
+list3 = ["house","money","dog","cat","bird"]
+#          0       1       2     3      4
+
+print(list1[4])
+print(list2[4])
+print(list3[4])
+
+words = ["rise", "sun", "glasses"]
+words[2] = "sunnglasses"
+
+print(words[2])
+
+
+nums = [20, 30, 40, 50, 60 ]
+nums[4] = 70
+
+print(nums[4])
+
+
+
+
+#mutable - ისეთი რამ რისი შეცვლაც შეიძლება
---
+++ DAY 18/Homework/homework.py
@@ -0,0 +1,13 @@
+list1 = [2, 51, 11, 13, 51, 100]
+print(list1)
+list1 = [-2, -51, -11, -13, -51, -100]
+print(list1)
+list1 = [2, 51, 11, 13, 51, 100]
+
+
+
+
+list1[5] = 200
+print(list1)
+list1[4] = 100
+print(list1)
