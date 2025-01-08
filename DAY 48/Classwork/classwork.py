392b2042f6e70612a90f876f188553b78b520e5c
mevludi15 <mevludigamer08@gmail.com>
2025-01-08 14:02:50 +0400

    second commit

---
+++ DAY 48/Classwork/classwork.py
@@ -0,0 +1,16 @@
+product = {
+    "name":"Christmas tree",
+    "height":"210sm",
+    "color":"green",
+    "laight":"True",
+    "laight_color":"yellow"
+}
+
+for key in product.keys():
+    print(key)
+
+for value in product.values():
+    print(value)
+
+for item in product.items():
+    print(item)
