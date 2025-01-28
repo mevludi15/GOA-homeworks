3b8b226d0aac0aba7d7bea658328277b74a2036f
mevludi15 <mevludigamer08@gmail.com>
2025-01-28 19:46:47 +0400

    second commit

---
+++ DAY 57/Classwork/Script.js
@@ -0,0 +1,8 @@
+// Number არის რიცხვი ისევე როგორც int HTML'ში
+// string არის ნებისმიერი სიმბოლოსგან შემდგაარი წინადადება იგივეა როგორც HTML'ში str
+
+// DOM არის HTML'ის უზარმაზარი დოკუმენტი რომელშიც შედის მთლიანი HTML'ის მონაცემები 
+
+let res = document.getElementById("p")
+
+console.log(res)
