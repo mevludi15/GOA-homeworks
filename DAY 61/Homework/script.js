705f0f08a69b7563955b8c9c9bf928e7d7e4df72
mevludi15 <mevludigamer08@gmail.com>
2025-02-08 22:35:04 +0400

    second commit

---
+++ DAY 61/Homework/script.js
@@ -0,0 +1,31 @@
+if (confirm("Are you an adult?")) {
+    alert("You are adult");
+} else {
+    alert("You are kid");
+}
+
+console.log(10 > 5);   
+console.log(5 < 10);   
+console.log(5 >= 5);  
+console.log(10 <= 5);  
+console.log("b" > "a");
+
+console.log(5 != "5");   
+console.log(10 != 20);   
+console.log(0 != 10); 
+console.log("apple" != "banana"); 
+console.log(20 != 13);
+
+console.log(5 + 10)
+console.log(10 - 20)
+console.log(12 * 2)
+console.log(4 / 2)
+console.log(20 - 10)
+
+// alert' საიტზე შესვლის თანავე ან მას შემდეგ გაბრთხილებას აძლევს მომხმარებელს მაშინ როდესაც
+// რაღაც სწორად არ მუშაობს 
+
+// pormpt'ის მეშვეობით მომხარებელს ვეუბნებით შემოიტანოს რაღაც ინფორმაცია საიტზე რის შემდეგაც შეგვიძლია
+// გამოვიტანოთ ეს ველიუ
+
+// confrim არის ფუნქცია რომელიც გამოყოფს ფანჯარას OK და Cancel ღილაკებით.
