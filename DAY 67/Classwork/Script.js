55867e5b2928bf6368049de2a3dac279c7795227
mevludi15 <mevludigamer08@gmail.com>
2025-02-22 15:27:37 +0400

    second commit

---
+++ DAY 67/Classwork/Script.js
@@ -0,0 +1,24 @@
+let listn = ['python','HTML','css','Java','JavaScript'];
+console.log(listn[0]);
+console.log(listn[1]);
+console.log(listn[2]);
+console.log(listn[3]);
+console.log(listn[4]);
+
+let myList = [10, "Hello", false];
+console.log(myList);
+
+let listn1 = new Array(4);
+
+// Array 4 ქმნის ლისტს რომელშიც შეგვიძლია შევიტანოთ 4 ველიუ ცარიელის დატოვების შემთხვევაში გამოგვიტანს რომ ერთი "ცვლადი" არის
+// ცარიელი. ვალიუების შეცვლა შეგვიძლია ინდექსებით და ასევე ველიუების გამოტანა შეგვიძლია ინდექსებით
+
+
+let myself = [];
+myself['name'] = "mevludi"
+myself['surname'] = "tavadze"
+myself['age'] = 15
+
+console.log(myself)
+
+// array'ს სიგრძე იმიტომ გამოდის 0 რომ ესენი არ მიეკუთვნება array'ს არამედ იქმნება ობიექტები
---
+++ DAY 67/Homework/Script.js
@@ -0,0 +1,17 @@
+console.log(Math.random() * 100)
+
+
+console.log(Math.max(1,2,20,15,3,50))
+console.log(Math.min(2,-1,214,2,5,12))
+
+
+console.log(Math.abs(10,10,10,10))
+
+
+x = 15.3
+console.log(Math.floor(x))
+console.log(Math.ceil(x))
+console.log(Math.round(x))
+
+y = 5
+console.log(Math.sqrt(5))
